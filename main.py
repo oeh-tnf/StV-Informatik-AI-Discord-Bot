d0d28414e29f7f458dba2b32f8f77114efac96a9
Jonathan Kudlich <jonathan.kudlich@gmx.at>
2021-11-24 16:55:02 +0100

    Fixed a mistake

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ roles_dict = {"1. Semester": '1️⃣'.encode('unicode-escape').decode('ASCII'),
               "Bachelor": '🇧'.encode('unicode-escape').decode('ASCII'),
               "Master": '🇲'.encode('unicode-escape').decode('ASCII'),
               "∞.Semester": '♾️'.encode('unicode-escape').decode('ASCII'),
-              "PhD:": '🅿️'.encode('unicode-escape').decode('ASCII'),
+              "PhD": '🎓'.encode('unicode-escape').decode('ASCII'),
               "Alumni": '🅰️'.encode('unicode-escape').decode('ASCII')}
 
 role_msg_id = 756093869187137537
