ddeaef780c4a96ca8b51ce6d9412eb314c94a964
byte-kraken <fehstrash@gmail.com>
2021-11-27 21:34:26 +0100

    Updated welcome message
    Added German version. Now references #rules.

--- main.py
+++ main.py
@@ -41,13 +41,14 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    await member.send(f"Welcome to the Informatik & AI Discord Server! Here you can find other students of the JKU Linz who are also studying Informatik or AI from your and other semesters. Although the server is primarily aimed at JKU students, everyone is welcome.\n\n"
-                      f"As you can see, there are categories for each semester (sorted by curriculum) and there is a text channel for each course. There you can ask questions to fellow students and discuss the subject. If a course consists of several parts (e.g. exercise and lecture), you can discuss both in the respective channel.\n\n\n"
-                      f"So that you don't get annoyed by the other categories, you can mute them by right clicking on them. (You can also hide all muted channel in the dropdown on the top left, dont forget to untick this if you want to see them again!) "
-                      f"If you want to appear here with your real name, you can do this with a nickname (click on the arrow next to the server name in the upper left corner and then click on 'Change nickname'). This only changes the name on this server, so you can remain anonymous on other servers.\n\n"
-                      f"Link to invite new people: https://discord.gg/wAek576\n\n"
-                      f"If you need some help or there are problems with the server (or with someone on the server), don't be afraid to someone from @ÖH.\n\n"
-                      f"If you want to assign yourself a role go to this message and click the appropriate reactions: https://discordapp.com/channels/370458917073059841/497699283772899348/756093869187137537 ")
+    await member.send(f"Welcome to the Informatik & AI Discord Server! Here you can find other students of the JKU Linz who are also studying Informatik or AI. Although the server is primarily aimed at JKU students, everyone is welcome.\n\n"
+                      f"Check out our #welcome-Channel to find out more about the structure of the server, and to assign yourself a role: https://discord.com/channels/370458917073059841/497699283772899348/632232644570251264 \n\n"
+                      f"Before posting, make sure you have read the server-rules in #rules! They include rules against discriminatory jokes, spamming and advertising: https://discord.com/channels/370458917073059841/911658031828328448/911940999666880572 \n\n"
+                      f"This server is hosted by the Student Union (ÖH) for Informatics and AI. If you need help or there are problems with the server (or with someone on the server), don't be afraid to tell someone from @ÖH. Enjoy your time here!\n\n\n"
+                      f"Willkommen am Informatik & AI Discord Server! Hier kannst du dich mit anderen Studierenden der JKU Linz austauschen, die ebenfalls Informatik oder AI studieren. Obwohl der Server sich primär an Studierende richtet, sind alle willkommen.\n\n"
+                      f"Sieh dir den #welcome-Channel an, um mehr über die Struktur des Servers zu lernen, und dir eine Rolle zuzuteilen: https://discord.com/channels/370458917073059841/497699283772899348/632232644570251264 \n\n"
+                      f"Bitte lies dir vor dem Posten die Server-Richtlinien in #rules durch! Sie beinhalten Regeln gegen diskriminierende Witze, Spamming und Werbung: https://discord.com/channels/370458917073059841/911658031828328448/911940999666880572 \n\n"
+                      f"Der Server wird von der Studienvertretung für Informatik & AI (ÖH) betrieben. Falls du Probleme mit dem Server (oder mit jemandem auf dem Server) hast, bitte melde dich einfach bei jemandem mit der @ÖH-Rolle. Wir wünschen dir viel Spaß am Server!")
 
 def add_or_remove(payload, mode):
     member = bot.get_guild(payload.guild_id).get_member(payload.user_id)
